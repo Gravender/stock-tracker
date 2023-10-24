face50dbb215e6c1d04219872cd14f3fe27ddd38
Noah Stevens <ss.noah@gmail.com>
2023-10-24 13:06:30 +1100

    feat: :sparkles: basic layout

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,5 +1,7 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').options} */
 const config = {
+  singleQuote: true,
+  semi: false,
   plugins: ["prettier-plugin-tailwindcss"],
 };
 
